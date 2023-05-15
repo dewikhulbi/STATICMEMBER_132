f5277e8ae7359853f9e3d76bba8f55de836a23d8
dewikhulbi <dewikhlb20@gmail.com>
2023-05-15 19:22:34 +0700

    return 0 mengembalikan nilai

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -11,3 +11,31 @@ public:
 	void cetakData();
 	void isiData();
 };
+//definisi member function
+angka::angka(int i) { //constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+
+angka::angka() { //destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::isiData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << "="; cin >> arr[i];
+	}
+	cout << endl;
+}
+
+int main() {
+	angka belajarcpp(3); //constructor dipanggil
+	angka* ptrBelajarcpp = new angka(5); //constructor dipanggil
+	delete ptrBelajarcpp; //destructor dipanggil
+
+	return 0;
+} //destructor dipanggil

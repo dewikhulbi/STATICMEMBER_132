c94eba86c0968e6884ac8a4becdf7a7141267275
dewikhulbi <dewikhlb20@gmail.com>
2023-05-15 19:25:10 +0700

    memasukan data mahasiswa

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -3,3 +3,40 @@
 using namespace std;
 
 class mahasiswa {
+private:
+	int nim;
+	string nama;
+public:
+	mahasiswa();
+	mahasiswa(int);
+	mahasiswa(string);
+	mahasiswa(int iNIm, string iNama);
+	void cetak();
+};
+
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = "";
+}
+mahasiswa::mahasiswa(int iNim) {
+	nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama) {
+	nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
+
+void mahasiswa::cetak() {
+	cout << endl << "Nim =" << nim << endl;
+	cout << "Nama =" << nama << endl;
+}
+int main() {
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("Indra");
+	mahasiswa mhs4("Fauzan");
